c8babcfe79004340f3e5d5842368d5ab39d499e0
wcchristian <christian.andersen@datasite.com>
2022-10-21 12:13:54 -0500

    Fixing up file loader, better comments

--- src/main/kotlin/org/example/adventofcode/util/FileLoader.kt
+++ src/main/kotlin/org/example/adventofcode/util/FileLoader.kt
@@ -5,13 +5,13 @@ import java.util.stream.Collectors
 @Suppress("UNCHECKED_CAST")
 object FileLoader {
 
-    @SuppressWarnings("unchecked")
     inline fun <reified T> loadFromFile(filePath: String): List<T> {
-        val fileText = FileLoader::class.java.getResource(filePath)!!.readText()
+        val fileText = FileLoader::class.java.getResource(filePath)!!.readText() // Just NPE if file can't be found
         return when(T::class) {
             Int::class -> fileText.lines().stream().map {it.toInt()}.collect(Collectors.toList()) as List<T>
             Float::class -> fileText.lines().stream().map { it.toFloat() }.collect(Collectors.toList()) as List<T>
-            else -> fileText.lines() as List<T>
+            String::class -> fileText.lines() as List<T>
+            else -> fileText.lines() as List<T> // Default is treated like strings
         }
     }
 }
